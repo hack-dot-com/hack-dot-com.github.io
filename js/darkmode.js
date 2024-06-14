fb5ed720cf1adb0831a3fdc53f17e7c875486d8d
admin <hackdotcomblog@gmail.com>
2024-06-14 23:26:40 +0300

    init

---
+++ js/darkmode.js
@@ -0,0 +1,26 @@
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Change the icons inside the button based on previous settings
+if (document.documentElement.classList.contains("dark")) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // toggle theme
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("color-theme", "light");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("color-theme", "dark");
+  }
+});
